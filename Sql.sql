f753d28486d0b3c911f8de7c602511c6c54fa2d1
ButterMellown <52692749+Butt-M@users.noreply.github.com>
2022-12-07 01:20:06 +0700

    Some SQL Test

---
+++ Sql.sql
@@ -0,0 +1,9 @@
+INSERT INTO customer 
+VALUES ('Karjo', '+6281315498'),
+('Antok','+62895648741'),
+('Slamet','+62856749422'),
+('Karminah','+628135797455');
+
+INSERT INTO status VALUES ('Ready To Use'),('Being Used');
+
+SELECT * FROM customer ORDER BY Name
